78d0aad231b175d28bfb98d6b567dc4aa8b7abb3
Silvia Garcia <garcia.s091@gmail.com>
2020-10-28 06:04:54 -0500

    Crear ejercicio 1 y 2

---
+++ Ejercicio_1/Ejercicios.js
@@ -0,0 +1,10 @@
+let N1; //Int
+let N2; // Int
+let Suma; //Int
+
+N1= 23;
+N2= 23;
+
+Suma = N1 + N2;
+
+alert('La suma de ' + N1 + ' + ' + N2 + ' es: ' + Suma)
---
+++ Ejercicio_2/Ejercicio2.js
@@ -0,0 +1,11 @@
+alert('Ejercicio 2')
+let N1; //Int
+let N2; // Int
+let Suma; //Int
+
+N1= 67;
+N2= 1;
+
+Suma = N1 + N2;
+
+alert('La suma es 2: '+ Suma)
