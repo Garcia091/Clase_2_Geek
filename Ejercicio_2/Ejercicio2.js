2479960402aaebfb7b5991fb67438d6ce563da1e
Silvia Garcia <garcia.s091@gmail.com>
2020-10-29 20:27:51 -0500

    funcion document.write

--- Ejercicio_2/Ejercicio2.js
+++ Ejercicio_2/Ejercicio2.js
@@ -3,9 +3,9 @@ let N1; //Int
 let N2; // Int
 let Suma; //Int
 
-N1= 67;
-N2= 1;
+N1 = 67;
+N2 = 1;
 
 Suma = N1 + N2;
-
-alert('La suma es 2: '+ Suma)
+document.write('La suma es 2: ' + Suma)
+alert('La suma es 2: ' + Suma)
